4aa8138367cee2c7cf3818d5e3a89ed2039c8bf3
Maxime Orefice <morefice@gitlab.com>
2020-07-15 16:19:00 -0400

    Add demo with coverage

--- spec/demo_spec.rb
+++ spec/demo_spec.rb
@@ -1,1 +1,11 @@
 require 'spec_helper'
+
+RSpec.describe Demo do
+  describe "#hello_world" do
+    subject { Demo.new }
+
+    it "returns hello world" do
+      expect(subject.hello_world).to eq("Hello World!")
+    end
+  end
+end
